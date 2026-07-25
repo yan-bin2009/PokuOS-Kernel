3a8e1c5a9ccae31f4cefdd67f12833938726463e
yan-bin2009 <yan20095642558@163.com>
2026-07-25 22:19:03 +0800

    全局更新：包括了统一代码风格，完善系统调用，用户态切换框架和交互式Shell

--- src/format.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# 批量将 .c .h .asm 文件的行首缩进统一为 8 格制表符
-
-find . -type f \( -name "*.c" -o -name "*.h" -o -name "*.asm" \) -exec sh -c '
-    for f do
-        unexpand --first-only -t 8 "$f" > "$f.tmp" && mv "$f.tmp" "$f"
-    done
-' sh {} +
